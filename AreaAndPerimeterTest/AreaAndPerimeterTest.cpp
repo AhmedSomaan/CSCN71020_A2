cee405203842c4b375bc9b33812eb3f7177f1613
Ahmed Somaan <ahmed_somaan@hotmail.com>
2024-10-16 19:27:30 -0400

    Created Unit tests for area and perimeter functions

---
+++ AreaAndPerimeterTest/AreaAndPerimeterTest.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" int getPerimeter(int* length, int* width);
+extern "C" int getArea(int* length, int* width);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace AreaAndPerimeterTest
+{
+	TEST_CLASS(AreaAndPerimeterTest)
+	{
+	public:
+		
+		TEST_METHOD(AreaTest)
+		{
+			int length = 5;
+			int width = 4;
+			int expectedResult = 20;
+			Assert::AreEqual(expectedResult, getArea(&length, &width));
+		}
+
+		TEST_METHOD(PerimeterTest)
+		{
+			int length = 5;
+			int width = 4;
+			int expectedResult = 18;
+			Assert::AreEqual(expectedResult, getPerimeter(&length, &width));
+		}
+	};
+}
